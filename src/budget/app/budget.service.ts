19962d4045cc5c8d2f2ebce31755fd329420084d
[AI7] 정재훈 <wjdwogns120523@gmail.com>
2024-04-28 17:12:03 +0900

    refact:불필요한 try catch 제거

--- src/budget/app/budget.service.ts
+++ src/budget/app/budget.service.ts
@@ -29,67 +29,21 @@ export class BudgetService implements IBudgetService {
   ) {}
 
   async createBudget(req: ReqBudgetDto): Promise<string> {
-    try {
-      const yearMonth = new Date(req.month) // month는 문자열
-      const existingBudget = await this.budgetRepository.findSameBudget(
-        yearMonth,
-        req.userId,
-      )
-      // 해당 달에 이미 예산이 있는지 확인
-      if (Object.keys(existingBudget).length > 0) {
-        throw new ConflictException(BUDGET_ALREADY_EXIST)
-      } else {
-        await Promise.all(
-          Object.entries(req.amount).map(async ([classification, budget]) => {
-            await this.budgetRepository.createBudget(
-              req.userId,
-              yearMonth,
-              Number(classification),
-              budget,
-            )
-          }),
-        )
-        return '예산 설정에 성공하였습니다.'
-      }
-    } catch (error) {
-      if (error instanceof ConflictException) {
-        throw error
-      } else {
-        throw new InternalServerErrorException('예산 설정에 실패했습니다.')
-      }
+    const existingBudget = await this.budgetRepository.findSameBudget(
+      new Date(req.month),
+      req.userId,
+    )
+    if (Object.keys(existingBudget).length > 0) {
+      throw new ConflictException(BUDGET_ALREADY_EXIST)
     }
+
+    await this.processBudget(req, 'createBudget')
+    return '예산 설정에 성공하였습니다.'
   }
 
   async updateBudget(req: ReqBudgetDto): Promise<string> {
-    try {
-      const yearMonth = new Date(req.month)
-      const existingBudget = await this.budgetRepository.findSameBudget(
-        yearMonth,
-        req.userId,
-      )
-      // 해당 달에 이미 예산이 있는지 확인
-      if (Object.keys(existingBudget).length > 0) {
-        await Promise.all(
-          Object.entries(req.amount).map(async ([classification, budget]) => {
-            await this.budgetRepository.updateBudget(
-              req.userId,
-              yearMonth,
-              Number(classification),
-              budget,
-            )
-          }),
-        )
-        return '예산 변경에 성공하였습니다.'
-      } else {
-        throw new NotFoundException(BUDGET_NOTFOUND)
-      }
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error
-      } else {
-        throw new InternalServerErrorException('예산 변경에 실패했습니다.')
-      }
-    }
+    await this.processBudget(req, 'updateBudget')
+    return '예산 변경에 성공하였습니다.'
   }
 
   async recommendBudget(req: ReqRecommendBudgetDto): Promise<object> {
@@ -110,5 +64,22 @@ export class BudgetService implements IBudgetService {
       message: '정상적으로 추천 예산이 생성되었습니다.',
       recommendedBudget,
     }
+  }
+
+  private async processBudget(
+    req: ReqBudgetDto,
+    message: string,
+  ): Promise<void> {
+    const yearMonth = new Date(req.month)
+    await Promise.all(
+      Object.entries(req.amount).map(async ([classification, budget]) => {
+        await this.budgetRepository[message](
+          req.userId,
+          yearMonth,
+          Number(classification),
+          budget,
+        )
+      }),
+    )
   }
 }
